11fe5f1ef698866f373da713b2c1a7300e102b0d
Hakeem Grillo <ladigrillo48@gmail.com>
2026-02-09 06:28:44 +0100

    playing around with awk

--- get-system-info/sysinfo.sh
+++ get-system-info/sysinfo.sh
@@ -1,12 +1,20 @@
 
 #!/bin/bash
 
-## Simple script to display system information
-
+## Simple script to display linux system information
+# // PLAYING AROUND with grep
  HOST=$( cat /etc/os-release | grep "^NAME" | cut -d '=' -f 2 | tr -d '"')
  ACTIVE_DAYS=$(uptime | grep -oP '\d+(?= day)')
+ HOST_VERSION=$( cat /etc/os-release | grep "^VERSION=" | cut -d '=' -f 2 | tr -d '"')
 
- echo -e  "Displaying system information for host: $HOST" 
+#  // PLAYING AROUND WITH AWK
+TOTAL_MEM=$( free -h | awk '/Mem/ {print $2}')
 
- echo -e "System has been up for $ACTIVE_DAYS day(s)"
+ echo  "Displaying system information for host: $HOST" 
+
+ echo "Version: $HOST_VERSION"
+
+ echo "Total Memory: $TOTAL_MEM"
+
+ echo "System has been up for $ACTIVE_DAYS day(s)"
 
